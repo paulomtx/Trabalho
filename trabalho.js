040948d64dea6d2778d308e51b85f9ffb8865dc0
PAULO LUIZ MELO TEXEIRA <PAULO0381@DIGITALCOLLEGE.LOCAL>
2024-07-08 16:51:11 -0300

    jokepo

--- trabalho.js
+++ trabalho.js
@@ -3,3 +3,16 @@ const opcoes={
     1:{nome: 'papel ', img:'papel.png',pedra: 'GANHOU', papel: 'EMPATE' ,tesoura:'PERDEU'},
     1:{nome: 'tesoura ', img:'tesoura.png',pedra: 'PERDEU', papel: 'GANHOU' ,tesoura:'EMPATE'},
 }
+
+let escolhaJogador={}
+let escolhaComputador={}
+
+
+function escolherJogada(num){
+    return opcoes[num]
+}
+
+let numComp = Math.floor((Math.random()*3)+1)
+escolhaComputador = opcoes[numComp]
+
+
