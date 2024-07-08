e3f4a828fd6f70221b8d4c55214196a86d3fac5f
PAULO LUIZ MELO TEXEIRA <PAULO0381@DIGITALCOLLEGE.LOCAL>
2024-07-08 16:41:53 -0300

    jokepo

---
+++ trabalho.js
@@ -0,0 +1,5 @@
+const opcoes={
+    1:{nome: 'pedra ', img:'pedra.png', pedra: 'EMPATE', papel: 'PERDEU' ,tesoura:'GANHOU'},
+    1:{nome: 'papel ', img:'papel.png',pedra: 'GANHOU', papel: 'EMPATE' ,tesoura:'PERDEU'},
+    1:{nome: 'tesoura ', img:'tesoura.png',pedra: 'PERDEU', papel: 'GANHOU' ,tesoura:'EMPATE'},
+}
